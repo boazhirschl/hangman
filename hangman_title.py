7711f0d8bb5802db7e8aaa2b8e25307d85c3bdf6
Boaz Hirschl <Boaz.Hirschl@gmail.com>
2023-09-05 11:21:54 +0300

    hangman_title

---
+++ hangman_title.py
@@ -0,0 +1,20 @@
+# print the hangman title
+
+
+print (""" 
+Welcome to the game Hangman
+    _    _
+   | |  | |
+   | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __
+   |  __  |/ _' | '_ \ / _' | '_ ' _ \ / _' | '_ \\
+   | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+   |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                        __/ |
+                       |___/
+""")
+from random import randrange
+
+import random
+trys = random.randint(5, 10) 
+print(trys)
+
