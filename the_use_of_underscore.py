6ec49f305bced39b7c134c9e2a0b98dd69e5cadb
Boaz Hirschl <Boaz.Hirschl@gmail.com>
2023-09-14 13:29:38 +0300

    another commit

---
+++ the_use_of_underscore.py
@@ -0,0 +1,16 @@
+# 3 little piglets
+# input 3 dig number_of_bricks report sum average and 
+number_of_bricks = int(input("Enter three digits (each digit for one pig):"))
+print ("with loop")
+digits = [int(d) for d in str(number_of_bricks)]
+print(digits)
+
+print ("with division")
+hundreds_digit = number_of_bricks // 100
+tens_digit = (number_of_bricks % 100) // 10
+ones_digit = number_of_bricks % 10
+digits = [hundreds_digit, tens_digit, ones_digit]
+print(digits)
+print ("results :sum,per piglet, reminader , is it divide124d evenly ")
+total = hundreds_digit + tens_digit + ones_digit
+print ( total  , total // 3 , total % 3, not (total % 3))
